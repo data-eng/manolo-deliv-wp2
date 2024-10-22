0e4e911d2c1a4ede2cb350726d0dee69d8914fbf
nataliakoliou <natalykoliou@yahoo.gr>
2024-10-22 15:35:40 +0300

    Visualize multiple noise estimations per channel

--- noise-analysis/src/utils.py
+++ noise-analysis/src/utils.py
@@ -52,60 +52,6 @@ def get_dir(*sub_dirs):
 
     return dir
 
-def visualize(type, values, labels, title, plot_func=None, coloring=None, names=None, classes=None, tick=False, path='static'):
-    """
-    Visualize (x,y) data points.
-
-    :param type: Type of visualization ('single-plot', 'multi-plot', or 'heatmap').
-    :param values: List of tuples or tuple containing the data points to visualize.
-    :param labels: Tuple containing labels for the x and y axes.
-    :param title: Title of the visualization.
-    :param plot_func: Plotting function (optional).
-    :param coloring: List or str containing colors for the plots (optional).
-    :param names: List of names for the plots (optional).
-    :param tick: Whether to display ticks on axes (optional).
-    :param classes: List of class names for labeling (optional).
-    :param path: Directory path to save the visualization.
-    """
-    x_label, y_label = labels
-    plt.figure(figsize=(10, 6))
-
-    if type == 'single-plot':
-        x_values, y_values = values
-        plot_func(x_values, y_values, color=coloring)
-
-        if tick:
-            plt.xticks(range(len(classes)), classes)
-            plt.yticks(range(len(classes)), classes)
-
-    elif type == 'multi-plot':
-        x_values, y_values = values
-
-        for i, (x_values, y_values) in enumerate(values):
-            plot_func(x_values, y_values, color=coloring[i], label=names[i])
-            plt.legend()
-
-        if tick:
-            plt.xticks(range(len(classes)), classes)
-            plt.yticks(range(len(classes)), classes)
-
-    elif type == 'heatmap':
-        x_values, y_values = values
-
-        cm = confusion_matrix(x_values, y_values)
-        cmap = sns.blend_palette(coloring, as_cmap=True)
-
-        sns.heatmap(cm, annot=True, fmt="d", cmap=cmap, xticklabels=classes, yticklabels=classes)
-
-    plt.xlabel(x_label)
-    plt.ylabel(y_label)
-    plt.title(title)
-    plt.tight_layout()
-
-    filename = f"{title.lower().replace(' ', '_')}.png"
-    plt.savefig(os.path.join(path, filename), dpi=300)
-    plt.close()
-
 def retain_if_greater_than_one(value):
     """
     Transformation function that retains the value if greater than 1, otherwise returns 0.
