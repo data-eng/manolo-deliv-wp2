9e8b84147828b73920e7f58512166f2f243b8c9b
nataliakoliou <natalykoliou@yahoo.gr>
2024-10-22 12:44:08 +0300

    Include logger in utils

--- noise-analysis/src/utils.py
+++ noise-analysis/src/utils.py
@@ -1,4 +1,25 @@
 import os
+import logging
+
+def get_logger(level='DEBUG'):
+    """
+    Create and configure a logger object with the specified logging level.
+
+    :param level: Logging level to set for the logger. Default is 'DEBUG'.
+    :return: Logger object configured with the specified logging level.
+    """
+    logger = logging.getLogger(__name__)
+
+    level_name = logging.getLevelName(level)
+    logger.setLevel(level_name)
+    
+    formatter = logging.Formatter('%(asctime)s:%(lineno)d:%(levelname)s:%(name)s:%(message)s')
+    
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+    
+    return logger
 
 def get_path(*dirs, filename):
     """
